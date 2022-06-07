14cad34412f710ed961689bc46bf1da82a557e1f
Shay Mayer <shaymayer2001@gmail.com>
2022-06-07 18:40:44 +0300

    Update extern_list.c

--- extern_list.c
+++ extern_list.c
@@ -7,20 +7,20 @@
 /* this function creates an empty list of externals (contains a garbage value) */
 extern_node *create_extern_list(){
     extern_node *list = (extern_node *) calloc(1, sizeof(extern_node)); /* the new list to return */
-	/* checking if the calloc function worked */
+    /* checking if the calloc function worked */
     if (list == NULL) {
         fprintf(stderr, "c language error: calloc failed");
         exit(1);
     }
 
     list->name = (char *) calloc(1, sizeof(char *));
-	/* checking if the calloc function worked */
+    /* checking if the calloc function worked */
     if (list->name == NULL) {
         fprintf(stderr, "c language error: calloc failed");
         exit(1);
     }
 
-	/* setting the place */
+    /* setting the place */
     list->place = 0;
 
     return list;
@@ -32,37 +32,37 @@ void add_extern_item(extern_node *list, char *name, long place) {
     char *name_temp; /* a temporary string for assigning the given name to the new element */
 
     new_node = (extern_node *) calloc(1, sizeof(extern_node));
-	/* checking if the calloc function worked */
+    /* checking if the calloc function worked */
     if (new_node == NULL) {
         fprintf(stderr, "c language error: calloc failed");
         exit(1);
     }
 
     new_node->name = (char *) calloc(1, sizeof(char *));
-	/* checking if the calloc function worked */
+    /* checking if the calloc function worked */
     if (new_node->name == NULL) {
         fprintf(stderr, "c language error: calloc failed");
         exit(1);
     }
 
     name_temp = (char *) malloc(strlen(name) + 1);
-	/* checking if the malloc function worked */
+    /* checking if the malloc function worked */
     if (name_temp == NULL) {
         fprintf(stderr, "c language error: malloc failed");
         exit(1);
     }
     strcpy(name_temp, name);
 
-	/* these 2 lines bellow are responsible for creating the new element */
+    /* these 2 lines bellow are responsible for creating the new element */
     new_node->name = name_temp;
     new_node->place = place;
 
-	/* reaching the end of the list */
+    /* reaching the end of the list */
     while (current->next != NULL) {
         current = current->next;
     }
 
-	/* adding the new element to the list */
+    /* adding the new element to the list */
     current->next = new_node;
 }
 
@@ -70,12 +70,12 @@ void add_extern_item(extern_node *list, char *name, long place) {
 void free_extern_list(extern_node *list) {
     extern_node *current; /* a pointer to the given list */
 
-	/* iterating through the list and freeing its elements */
+    /* iterating through the list and freeing its elements */
     while (list != NULL) {
         current = list;
         list = list->next; /* moving to the next element */
 
-		/* freeing the variables */
+	/* freeing the variables */
         free(current->name);
         free(current);
     }
