5644e52371162e01480ab054fdff3a0013628724
Shay Mayer <shaymayer2001@gmail.com>
2022-06-07 18:41:50 +0300

    Update file_writer.c

--- file_writer.c
+++ file_writer.c
@@ -22,13 +22,13 @@ void write_object_file(char *file_name , memory_cell *ic_memory_table, memory_ce
         return;
     }
 
-	/* writing amount of cells, of the data and of the insturction */
+    /* writing amount of cells, of the data and of the insturction */
     fprintf(ob_file, "     %ld %ld\n", icf - 100, dcf);
 
-	/* first element of the list is a garbage value */
+    /* first element of the list is a garbage value */
     ic_memory_table = ic_memory_table->next;
 
-	/* writing the cell of the instructions */
+    /* writing the cell of the instructions */
     while (ic_memory_table != NULL) {
         if (counter % 4 == 0 && ic_memory_table != NULL) { /* creating a new line when we are after writing 4 cells */
             if(counter != 0)
@@ -48,10 +48,10 @@ void write_object_file(char *file_name , memory_cell *ic_memory_table, memory_ce
         ic_memory_table = ic_memory_table->next; /* moving to next element */
     }
 
-	/* first element of the list is a garbage value */
+    /* first element of the list is a garbage value */
     dc_memory_table = dc_memory_table->next;
 
-	/* writing the data to the file */
+    /* writing the data to the file */
     while (dc_memory_table != NULL) {
         if (counter % 4 == 0 && dc_memory_table != NULL) { /* creating a new line when we are after writing 4 cells */
             if(counter != 0)
@@ -71,7 +71,7 @@ void write_object_file(char *file_name , memory_cell *ic_memory_table, memory_ce
         dc_memory_table = dc_memory_table->next; /* moving to next element */
     }
 
-	/* freeing the variables */
+    /* freeing the variables */
     free(final_filename);
     fclose(ob_file);
 }
@@ -85,7 +85,7 @@ void write_blank_object_file(char *file_name){
     FILE *ob_file;
 
     ob_file = fopen(final_filename, "w");
-	/* checking if the malloc function worked */
+    /* checking if the malloc function worked */
     if (ob_file == NULL) {
         fprintf(stderr, "c language error: can't create the file");
         free(final_filename);
@@ -94,7 +94,7 @@ void write_blank_object_file(char *file_name){
 
     fprintf(ob_file, "     0 0"); /* writing the value to the file */
 
-	/* freeing the variables */
+    /* freeing the variables */
     free(final_filename);
     fclose(ob_file);
 }
@@ -104,24 +104,24 @@ void write_entry_file(char *file_name, symbol_table *table){
     char *final_filename = add_extension_to_file(file_name, ".ent");
 
     ent_file = fopen(final_filename, "w");
-	/* checking if the malloc function worked */
+    /* checking if the malloc function worked */
     if (ent_file == NULL) {
         fprintf(stderr, "c language error: can't create the file");
         free(final_filename);
         return;
     }
 
-	/* first element of the list is a garbage value */
+    /* first element of the list is a garbage value */
     table = table->next;
 	
-	/* writing the data to the file */
+    /* writing the data to the file */
     while (table != NULL) {
         fprintf(ent_file, "%s ", table->name);
         fprintf(ent_file, "%04ld\n", table->address);
         table = table->next; /* moving to next element */
     }
 
-	/* freeing the variables */
+    /* freeing the variables */
     free(final_filename);
     fclose(ent_file);
 }
@@ -137,17 +137,17 @@ void write_extern_file(char *file_name, extern_node *list){
         return;
     }
 
-	/* first element of the list is a garbage value */
+    /* first element of the list is a garbage value */
     list = list->next;
 
-	/* writing the data to the file */
+    /* writing the data to the file */
     while (list != NULL) {
         fprintf(ext_file, "%s ", list->name);
         fprintf(ext_file, "%04ld\n", list->place);
         list = list->next; /* moving to next element */
     }
 
-	/* freeing the variables */
+    /* freeing the variables */
     free(final_filename);
     fclose(ext_file);
 }
